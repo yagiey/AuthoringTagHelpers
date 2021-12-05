97ebfb05e54c64f9e78bda0c573e8b885c204f79
yagiey <yagiey@gmail.com>
2021-12-05 21:40:14 +0900

    add WebsiteInformationTagHelper

---
+++ AuthoringTagHelpers/Models/WebsiteContext.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthoringTagHelpers.Models
+{
+    public class WebsiteContext
+    {
+        public Version Version { get; set; }
+        public int CopyrightYear { get; set; }
+        public bool Approved { get; set; }
+        public int TagsToShow { get; set; }
+    }
+}
