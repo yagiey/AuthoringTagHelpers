54536a42d1a2633e5a44e0a3fa26aaf4e43461eb
yagiey <yagiey@gmail.com>
2021-12-05 18:20:05 +0900

    add EmailTagHelper

--- AuthoringTagHelpers/Controllers/HomeController.cs
+++ AuthoringTagHelpers/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace AuthoringTagHelpers.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            ViewData["Title"] = nameof(Contact);
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
