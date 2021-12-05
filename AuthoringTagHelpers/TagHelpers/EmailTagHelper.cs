dfc13e28ca9f23fe1c91206b69cb7c42635bc327
yagiey <yagiey@gmail.com>
2021-12-05 21:02:10 +0900

    support attribute mail-to

--- AuthoringTagHelpers/TagHelpers/EmailTagHelper.cs
+++ AuthoringTagHelpers/TagHelpers/EmailTagHelper.cs
@@ -5,9 +5,19 @@ namespace AuthoringTagHelpers.TagHelpers
 {
     public class EmailTagHelper : TagHelper
     {
+        private const string EmailDomain = "contoso.com";
+
+        // Can be passed via <email mail-to="..." />. 
+        // PascalCase gets translated into kebab-case.
+        public string MailTo { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";    // Replaces <email> with <a> tag
+
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
         }
     }
 }
